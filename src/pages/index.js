ee569a5685fcb3af6f2fec224632840e53bea4af
Michael <checomichael2@gmail.com>
2019-04-17 23:02:08 -0400

    removed default styling + image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,14 @@ import React from 'react'
 import { Link } from 'gatsby'
 import styled from '@emotion/styled'
 import Layout from '../components/layout'
-import Image from '../components/image'
 
 const H1 = styled.h1`
   color: red;
 `
 const IndexPage = () => (
   <Layout>
-    <H1>
-      Hello {'🚀'}
-    </H1>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
+    <H1>Hello {'🚀'}</H1>
+
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
