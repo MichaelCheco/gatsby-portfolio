5b49d880a91e50a6e622fe4dd5df4db90edda863
Michael <checomichael2@gmail.com>
2019-04-20 00:16:14 -0400

    basic react spring usage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { Link } from 'gatsby'
 // import styled from '@emotion/styled'
 import Layout from '../components/layout'
-
+import { useSpring, animated } from 'react-spring'
 const Home = () => {
+  const fadeIn = useSpring({ opacity: 1, from: { opacity: 0 } })
   return (
     <Layout>
-      <h1>Hello {'🚀'}</h1>
+      <animated.h1 style={fadeIn}>Hello {'🚀'}</animated.h1>
 
       <Link to="/page-2/">Go to page 2</Link>
     </Layout>
