727f73fd3002b4b00de17a4cfa1e07087c5f3c11
Michael <checomichael2@gmail.com>
2019-04-22 23:45:03 -0400

    beginning layout for project display section

---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from '@emotion/styled'
+const Container = styled.div`
+  background-color: #11ffbd;
+  margin-top: 160px;
+  height: 300px;
+`
+const Projects = () => {
+  return <Container>PROJECTS</Container>
+}
+
+export default Projects
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,21 +4,18 @@ import styled from '@emotion/styled'
 import Layout from '../components/layout'
 import { useSpring, animated } from 'react-spring'
 import { GlobalStyles } from '../utils.js/reset'
+import Projects from '../components/Projects'
 const Div = styled.div`
   border: 3px solid red;
   grid-row: 2 / span 2;
   width: 50%;
   height: 100%;
-  /* width: 45%; */
-  /* height: 30vh; */
 `
 const Container = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 1fr 1fr 1fr 1fr;
   justify-items: center;
-  /* align-items: center; */
-  border: 5px solid black;
   width: 100%;
   height: 100vh;
 `
@@ -42,9 +39,26 @@ const Home = () => {
         <circle fill="white" cx="200" cy="100" r="100" />
       </svg>
       <Container>
-        <Div>Hello</Div>
-        <Div>Goodbye</Div>
+        <Div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+          corrupti perspiciatis voluptatem itaque doloremque iure, eveniet odit
+          laboriosam distinctio reprehenderit voluptates debitis dolore, fuga
+          eligendi esse ipsum eos neque quisquam. Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Repudiandae magni perspiciatis, quaerat
+          aperiam qui molestiae, numquam dicta aliquid nostrum earum distinctio
+          maxime ex, quis velit. Quam blanditiis excepturi fugit sint.
+        </Div>
+        <Div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+          corrupti perspiciatis voluptatem itaque doloremque iure, eveniet odit
+          laboriosam distinctio reprehenderit voluptates debitis dolore, fuga
+          eligendi esse ipsum eos neque quisquam. Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Repudiandae magni perspiciatis, quaerat
+          aperiam qui molestiae, numquam dicta aliquid nostrum earum distinctio
+          maxime ex, quis velit. Quam blanditiis excepturi fugit sint.
+        </Div>
       </Container>
+      <Projects />
     </Layout>
   )
 }
