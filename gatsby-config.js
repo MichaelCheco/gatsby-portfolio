48a4655f62472c0bde3b9014cec103b92b4ee568
Michael <checomichael2@gmail.com>
2019-05-04 22:38:50 -0400

    changed favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
         background_color: '#EA526F',
         theme_color: '#EA526F',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/me.jpg', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
